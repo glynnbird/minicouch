0e9cb40ffbea571804c346b7d6a27c4737aaf0c1
Glynn Bird <glynn.bird@gmail.com>
2025-09-23 14:52:48 +0100

    add another function wrapper to avoid reparsing the url

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
-export default function minicouch(path = '/') {
-
-  // parse the URL to extract creds
-  const parsedURL= new URL(process.env.COUCH_URL)
-  const plainURL = `${parsedURL.origin}${parsedURL.pathname}`
-  let auth = ''
-  if (parsedURL.username && parsedURL.password) {
-    auth = 'Basic ' + Buffer.from(`${parsedURL.username}:${parsedURL.password}`).toString('base64')
+export default function () {
+  function minicouch(path = '/') {
+    return new Proxy(() => path, {
+      get(target, prop, receiver) {
+        const url = minicouch(path + encodeURIComponent(prop) + '/')
+        return url
+      },
+      async apply(target, thisArg, argumentsList) {
+        let builderPath = target().replace(/\/$/, '')
+        const opts = {
+          headers: {
+            'content-type': 'application/json',
+            'user-agent': 'minicouch',
+            'Authorization': auth || undefined
+          },
+          method: 'get'
+        }
+        Object.assign(opts, argumentsList[0] || {})
+        if (opts.path) {
+          builderPath += '/' + opts.path
+          delete opts.path
+        }
+        let url = new URL(builderPath, plainURL).toString()
+        if (typeof opts.qs === 'object') {
+          ['startkey', 'endkey', 'key', 'keys', 'start_key', 'end_key'].forEach(function (key) {
+            if (key in opts.qs) {
+              opts.qs[key] = JSON.stringify(opts.qs[key])
+            }
+          })
+          url += '?' + new URLSearchParams(opts.qs).toString()
+          delete opts.qs
+        }
+        if (opts.body && typeof body !== 'string') {
+          opts.body = JSON.stringify(opts.body)
+        }
+        const response = await fetch(url, opts)
+        return response.json()
+      }
+    })
   }
 
-  return new Proxy(() => path, {
-    get(target, prop, receiver) {
-      const url = minicouch(path + encodeURIComponent(prop) + '/')
-      return url
-    },
+  // parse the URL to extract creds
+  const parsedURL = new URL(process.env.COUCH_URL)
+  const plainURL = `${parsedURL.origin}${parsedURL.pathname}`
+  let auth = parsedURL.username && parsedURL.password ? 'Basic ' + Buffer.from(`${parsedURL.username}:${parsedURL.password}`).toString('base64') : ''
 
-    async apply(target, thisArg, argumentsList) {
-      let builderPath = target().replace(/\/$/, '') 
-      const opts = {
-        headers: {
-          'content-type': 'application/json',
-          'user-agent': 'minicouch',
-          'Authorization': auth || undefined
-        },
-        method: 'get'
-      }
-      Object.assign(opts, argumentsList[0] || {})
-      if (opts.path) {
-        builderPath += '/' + opts.path
-        delete opts.path
-      }
-      let url = new URL(builderPath, plainURL).toString()
-      if (typeof opts.qs === 'object') {
-        ['startkey', 'endkey', 'key', 'keys', 'start_key', 'end_key'].forEach(function (key) {
-          if (key in opts.qs) {
-            opts.qs[key] = JSON.stringify(opts.qs[key])
-          }
-        })
-        url += '?' + new URLSearchParams(opts.qs).toString()
-        delete opts.qs
-      }
-      if (opts.body && typeof body !== 'string') {
-        opts.body = JSON.stringify(opts.body)
-      }
-      const response = await fetch(url, opts)
-      return response.json()
-    }
-  })
+  return minicouch('/')
 }
+
 
 
 
