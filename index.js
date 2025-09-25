1aaa751cf812ff6163d9b70f422157c11b135c9e
Glynn Bird <glynn.bird@gmail.com>
2025-09-25 16:38:50 +0100

    1.1.1

--- index.js
+++ index.js
@@ -3,17 +3,12 @@ const MIME_JSON = 'application/json'
 export default function () {
   function minicouch(path = '/') {
     return new Proxy(() => path, {
-      get(target, prop, receiver) {
+      get(_, prop) {
         return minicouch(path + encodeURIComponent(prop) + '/')
       },
-      async apply(target, thisArg, argumentsList) {
-        let builderPath = target().replace(/\/$/, '')
-        const opts = {
-          headers: { 'content-type': MIME_JSON, 'user-agent': `${pkg.name}/${pkg.version}`, authorization: auth || undefined },
-          method: 'get'
-        }
-        Object.assign(opts, argumentsList[0] || {})
-        let url = new URL(builderPath, plainURL).toString()
+      async apply(target, _, args) {
+        const opts = { ...defaultOpts, ...(args[0] || {}) }
+        let url = new URL(target().replace(/\/$/, ''), plainURL).toString()
         if (typeof opts.qs === 'object') {
           ['startkey', 'endkey', 'key', 'keys', 'start_key', 'end_key'].forEach(function (key) {
             if (key in opts.qs) opts.qs[key] = JSON.stringify(opts.qs[key])
@@ -21,11 +16,8 @@ export default function () {
           url += '?' + new URLSearchParams(opts.qs).toString()
           delete opts.qs
         }
-        if (opts.body && typeof body !== 'string' && opts.headers['content-type'] === MIME_JSON) {
-          opts.body = JSON.stringify(opts.body)
-        }
+        opts.body = typeof opts.body === 'object' && opts.headers['content-type'] === MIME_JSON ? JSON.stringify(opts.body) : opts.body
         const response = await fetch(url, opts)
-        response.output = ''
         const contentType = response.headers.get('content-type')
         if (opts.method === 'head') {
           response.output = Object.fromEntries(response.headers)
@@ -36,16 +28,15 @@ export default function () {
         } else {
           response.output = Buffer.from(await response.arrayBuffer())
         }
-        if (response.ok)
-          return response.output
-        else
-          throw new Error(response.output?.reason || response.output?.error || 'couch returned ' + response.status)
+        if (response.ok) return response.output
+        throw new Error(response.output?.reason || response.output?.error || `couch returned ${response.status}`)
       }
     })
   }
-  const parsedURL = new URL(process.env.COUCH_URL)
-  const plainURL = `${parsedURL.origin}${parsedURL.pathname}`
-  let auth = parsedURL.username && parsedURL.password ? 'Basic ' + Buffer.from(`${parsedURL.username}:${parsedURL.password}`).toString('base64') : ''
+  const { origin, pathname, username, password } = new URL(process.env.COUCH_URL)
+  const plainURL = `${origin}${pathname}`
+  let auth = username && password ? 'Basic ' + Buffer.from(`${username}:${password}`).toString('base64') : ''
+  const defaultOpts = { headers: { 'content-type': MIME_JSON, 'user-agent': `${pkg.name}/${pkg.version}`, authorization: auth || undefined } }
   return minicouch('/')
 }
 
